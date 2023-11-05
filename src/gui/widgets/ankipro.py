99ab3586814d3e79e8679d4bf140220a74bec6c9
Abdo <abdo@abdnh.net>
2023-11-05 12:54:33 +0300

    Read AnkiPro email & password from env vars for testing

--- src/gui/widgets/ankipro.py
+++ src/gui/widgets/ankipro.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any, Optional
 
 from aqt.qt import *
@@ -10,6 +11,10 @@ class AnkiProWidget(ImporterWidget):
     def setup_ui(self) -> None:
         self.form = Ui_Form()
         self.form.setupUi(self)
+        if email := os.environ.get("ANKIPRO_EMAIL"):
+            self.form.email.setText(email)
+        if password := os.environ.get("ANKIPRO_PASSWORD"):
+            self.form.password.setText(password)
 
     def on_import(self) -> Optional[dict[str, Any]]:
         return {

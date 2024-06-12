ab1245ea3d2d981457bd1596f00864e7ab793b0f
Abdo <abdo@abdnh.net>
2024-06-12 15:32:52 +0300

    Update errors.py from add-on template

--- src/errors.py
+++ src/errors.py
@@ -1,16 +1,36 @@
-import ankiutils
-import ankiutils.errors
-from aqt import gui_hooks
+from anki.hooks import wrap
+from aqt import gui_hooks, mw
 from aqt.qt import *
 
 from .config import config
 from .consts import consts
 from .log import logger
 
+REGISTERED_ERROR_HANDLER = False
+
 
 def _on_profile_did_open() -> None:
-    ankiutils.errors.setup_error_handler(consts, config, logger)
+    global REGISTERED_ERROR_HANDLER
+
+    if not REGISTERED_ERROR_HANDLER:
+        try:
+            import ankiutils.errors
+
+            ankiutils.errors.setup_error_handler(consts, config, logger)
+            REGISTERED_ERROR_HANDLER = True
+        except ImportError:
+            logger.warning("ankiutils.errors not found; error handling is disabled.")
+
+
+def _before_exit() -> None:
+    import atexit
+    import logging
+
+    # Fix 'RuntimeError: wrapped C/C++ object of type ErrorHandler has been deleted' on shutdown
+    atexit.unregister(logging.shutdown)
+    logging.shutdown()
 
 
 def setup_error_handler() -> None:
     gui_hooks.profile_did_open.append(_on_profile_did_open)
+    mw.cleanupAndExit = wrap(mw.cleanupAndExit, _before_exit, "before")  # type: ignore

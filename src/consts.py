7c81f230919e925bed039f24f1add84840c53239
RobTheFiveNine <robthefivenine@proton.me>
2025-05-26 16:19:18 +0100

    Update user agent

--- src/consts.py
+++ src/consts.py
@@ -2,4 +2,4 @@ from ankiutils.consts import get_consts
 
 consts = get_consts(__name__)
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"

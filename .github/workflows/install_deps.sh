e061aae49d494446b1a0e5e4f96185b519d395e2
Abdo <abdo@abdnh.net>
2026-02-14 17:30:59 +0300

    Remove old workflow

--- .github/workflows/install_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pip install uv
-uv sync
-sudo apt-get update
-sudo apt install libxcb-xinerama0 libxcb-cursor0 libegl1
-curl -fsSL https://dprint.dev/install.sh | sh -s > /dev/null 2>&1
-echo "$HOME/.dprint/bin" >> $GITHUB_PATH
